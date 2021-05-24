bd1b95587a50498ac64a32dc7300092b10091aec
autvincere <aut.vincere@gmail.com>
2021-05-23 22:36:50 -0400

    webpack suspense and lazy is included

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route,Redirect } from 'react-router-dom'
-import Favorites from '../components/Favorites'
+import React, { Suspense, lazy } from 'react'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import Pokemons from '../components/Pokemons'
-import Search from '../components/Search'
+// import Favorites from '../components/Favorites'
+//import Search from '../components/Search'
 import Layout from '../components/Layout'
+import Loader from '../components/Loader';
+
+const Search = lazy(() => import('../components/Search'))
+const Favorites = lazy(() => import('../components/Favorites'))
 
 export const AppRouter = () => {
      return (
           <BrowserRouter>
                <Layout>
                     <Switch>
-                         <Route exact path='/favorites' component={Favorites} />
-                         <Route exact path='/search' component={Search} />
-                         <Route exact path='/pokemons' component={Pokemons} />
-                         <Route exact path='/'>
-                              <Redirect to='/pokemons' />
-                         </Route>
-                         <Route exact path='*'> <h1>404 not found</h1> </Route>
+                         <Suspense fallback={
+                              <div>
+                                   <Loader />
+                              </div>
+                         }>
+                              <Route exact path='/favorites' component={Favorites} />
+                              <Route exact path='/search' component={Search} />
+                              <Route exact path='/pokemons' component={Pokemons} />
+                              <Route exact path='/'>
+                                   <Redirect to='/pokemons' />
+                              </Route>
+                              {/* <Route exact path='*'> <h1>404 not found</h1> </Route> */}
+                         </Suspense>
                     </Switch>
                </Layout>
           </BrowserRouter>
